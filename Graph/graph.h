cdf68f466642c66a90bdaa9f87f750f3a943749e
gabo1305 <gabrielsolcor@gmail.com>
2020-09-25 16:18:39 -0600

    Add LinkedList

---
+++ Graph/graph.h
@@ -0,0 +1,18 @@
+/**
+ * @filee graph.h
+ * @author Gabriel Solano Coronado
+ * @version 1.0
+ * @date 25/9/2020
+ * @title grafo
+ */
+
+#ifndef INC_1EXTRACLASS_GRAPH_H
+#define INC_1EXTRACLASS_GRAPH_H
+
+
+class graph {
+
+};
+
+
+#endif //INC_1EXTRACLASS_GRAPH_H
